151b3f890e2fb5ec78602a0d0027deabd53f78f8
RunarX Bjarnason <rbjarnax@rbjarnax-mac01.local>
2013-10-31 13:38:36 -0700

    Simplify access by using maps instead of intmaps

--- src/main/scala/com/intel/graphs/Graph.scala
+++ src/main/scala/com/intel/graphs/Graph.scala
@@ -1,38 +1,46 @@
 import scala.collection.immutable.IntMap
 
 import scalaz.{Node => _, _}
-import scalaz.std.anyVal._
-import scalaz.syntax.equal._
+import scalaz.syntax.std.map._
 
-import Graph._
+/** A module of graphs, parameterized on the type of the `Node` unique identifier */
+class Graphs[Node] {
 
-/** The view of a graph focused on the context surrounding a particular node. */
-case class Context[A,B](inEdges: Adj[B], vertex: Node, label: A, outEdges: Adj[B])
+  /** The view of a graph focused on the context surrounding a particular node. */
+  case class Context[A,B](inEdges: Adj[B], vertex: Node, label: A, outEdges: Adj[B]) {
 
-/** Unlabeled Edge */
-case class Edge(from: Node, to: Node)
+    /** All the incoming edges plus outgoing edges to self */
+    def ins: Adj[B] = inEdges ++ outEdges.filter(_._2 == vertex)
 
-/** Labeled Edge */
-case class LEdge[A](from: Node, to: Node, label: A)
+    /** All the outgoing edges plus incoming edges to self */
+    def outs: Adj[B] = outEdges ++ inEdges.filter(_._2 == vertex)
+  }
 
-/** Labeled Node */
-case class LNode[A](vertex: Node, label: A)
+  /** Unlabeled Edge */
+  case class Edge(from: Node, to: Node)
 
-/** The label, predecessors, and successors of a given node */
-case class GrContext[A,B](inEdges: IntMap[Vector[B]], label: A, outEdges: IntMap[Vector[B]])
+  /** Labeled Edge */
+  case class LEdge[A](from: Node, to: Node, label: A)
 
-/**
- * The decomposition of a graph into a detached context focused on one node
- * and the rest of the graph.
- */
-case class Decomp[A,B](ctx: Option[Context[A,B]], rest: Graph[A,B])
+  /** Labeled Node */
+  case class LNode[A](vertex: Node, label: A)
 
-/** The same as `Decomp`, only more sure of itself */
-case class GDecomp[A,B](ctx: Context[A, B], rest: Graph[A, B])
+  /** The label, predecessors, and successors of a given node */
+  case class GrContext[A,B](inEdges: Map[Node, Vector[B]],
+                            label: A,
+                            outEdges: Map[Node, Vector[B]])
 
-object Graph {
+  /**
+   * The decomposition of a graph into a detached context focused on one node
+   * and the rest of the graph.
+   */
+  case class Decomp[A,B](ctx: Option[Context[A,B]], rest: Graph[A,B])
+
+  /** The same as `Decomp`, only more sure of itself */
+  case class GDecomp[A,B](ctx: Context[A, B], rest: Graph[A, B])
+
   /** The internal representation of a graph */
-  type GraphRep[A,B] = IntMap[GrContext[A,B]]
+  type GraphRep[A,B] = Map[Node, GrContext[A,B]]
 
   /** Unlabeled node */
   type Node = Int
@@ -57,24 +65,20 @@ object Graph {
   def buildGraph[A,B](ctxs: Seq[Context[A,B]]): Graph[A,B] =
     ctxs.foldLeft(empty[A,B])(_ & _)
 
-  /** Modify the value under a key in an intmap with the function `f` */
-  def adjust[A](m: IntMap[A], i: Int, f: A => A): IntMap[A] =
-    m.get(i).map(v => m.updated(i, f(v))).getOrElse(m)
-
   def clear[A,B](g: GraphRep[A,B], v: Node, ns: Vector[Node],
                  f: GrContext[A,B] => GrContext[A,B]): GraphRep[A,B] =
-    if (ns.isEmpty) g else clear(adjust(g, ns.head, f), v, ns.tail, f)
+    if (ns.isEmpty) g else clear(g.alter(ns.head)(_ map f), v, ns.tail, f)
 
   def addSucc[A,B](g: GraphRep[A, B], v: Node, lps: Vector[(B, Node)]): GraphRep[A,B] =
-    if (lps.isEmpty) g else addSucc(adjust(g, lps.head._2, { (x: GrContext[A,B]) => x match {
+    if (lps.isEmpty) g else addSucc(g.alter(lps.head._2)(_ map { (x: GrContext[A,B]) => x match {
       case GrContext(ps, lp, ss) =>
-        GrContext(ps, lp, ss.updateWith(v, Vector(lps.head._1), _ ++ _))
+        GrContext(ps, lp, ss.insertWith(v, Vector(lps.head._1))(_ ++ _))
     }}), v, lps.tail)
 
   def addPred[A,B](g: GraphRep[A, B], v: Node, lss: Vector[(B, Node)]): GraphRep[A,B] =
-    if (lss.isEmpty) g else addPred(adjust(g, lss.head._2, { (x: GrContext[A,B]) => x match {
+    if (lss.isEmpty) g else addPred(g.alter(lss.head._2)(_ map { (x: GrContext[A,B]) => x match {
       case GrContext(ps, lp, ss) =>
-        GrContext(ps.updateWith(v, Vector(lss.head._1), _ ++ _), lp, ss)
+        GrContext(ps.insertWith(v, Vector(lss.head._1))(_ ++ _), lp, ss)
     }}), v, lss.tail)
 
   def clearPred[A,B](g: GraphRep[A,B], v: Node, ns: Vector[Node]): GraphRep[A,B] =
@@ -84,197 +88,229 @@ object Graph {
     clear(g, v, ns, { case GrContext(ps, l, ss) => GrContext(ps, l, ss - v) })
 
   /** Turn an intmap of vectors of labels into an adjacency list of labeled edges */
-  def toAdj[B](bs: IntMap[Vector[B]]): Adj[B] = bs.toVector.flatMap {
+  def toAdj[B](bs: Map[Node, Vector[B]]): Adj[B] = bs.toVector.flatMap {
     case (n, ls) => ls.map(m => (m, n))
   }
 
   /** Turn an adjacency list of labeled edges into an intmap of vectors of labels */
-  def fromAdj[B](adj: Adj[B]): IntMap[Vector[B]] =
+  def fromAdj[B](adj: Adj[B]): Map[Node, Vector[B]] =
     adj.foldLeft(IntMap.empty[Vector[B]]) {
       case (m, (b, n)) => m + (n -> (m.get(n).toVector.flatten :+ b))
     }
-}
-
-/**
- * An efficient implementation of an inductive graph using `IntMap` (i.e. Patricia Trees).
- * Nodes are labeled with `A`, and edges are labeled with `B`.
- */
-case class Graph[A,B](rep: GraphRep[A,B]) {
-  def isEmpty = rep.isEmpty
 
   /**
-   * Returns a context focused on the given node, if present,
-   * and the graph with that node removed.
+   * An efficient implementation of an inductive graph using `IntMap` (i.e. Patricia Trees).
+   * Nodes are labeled with `A`, and edges are labeled with `B`.
    */
-  def decomp(n: Node): Decomp[A, B] = rep.get(n) match {
-    case None => Decomp(None, this)
-    case Some(GrContext(p, label, s)) =>
-      val g1 = rep - n
-      val pp = p - n
-      val sp = s - n
-      val g2 = clearPred(g1, n, sp.keys.toVector)
-      val g3 = clearSucc(g2, n, pp.keys.toVector)
-      Decomp(Some(Context(toAdj(pp), n, label, toAdj(s))), Graph(g3))
-  }
+  case class Graph[A,B](rep: GraphRep[A,B]) {
+    def isEmpty = rep.isEmpty
 
-  /**
-   * Merge the given context into the graph. The context consists of a vertex, its label,
-   * its successors, and its predecessors.
-   */
-  def &(ctx: Context[A,B]): Graph[A,B] = {
-    val Context(p, v, l, s) = ctx
-    val g1 = rep + (v -> GrContext(fromAdj(p), l, fromAdj(s)))
-    val g2 = addSucc(g1, v, p)
-    val g3 = addPred(g2, v, s)
-    Graph(g3)
-  }
-
-  /*
-  def addNode(n: LNode[A]): Graph[A,B] =
-    &((Vector(), n._1, n._2, Vector()))
-
-  def addEdge(e: LEdge[B]): Graph[A,B] = {
-    val (v, w, l) = e
-    val (Some((pr, _, la, su)), gp) = decomp(e._1)
-    gp & ((pr, v, la, (l, w) +: su))
-  }
-  */
-
-  /** Add a node to this graph */
-  def addNode(n: LNode[A]): Graph[A,B] = {
-    val LNode(v, l) = n
-    Graph(rep + (v -> GrContext(IntMap.empty, l, IntMap.empty)))
-  }
-
-  /** Add an edge to this graph */
-  def addEdge(e: LEdge[B]): Graph[A,B] = {
-    val LEdge(v, w, l) = e
-    def addSuccP(p: GrContext[A,B]) = {
-      val GrContext(ps, lp, ss) = p
-      GrContext(ps, lp, ss.updateWith(w, Vector(l), _ ++ _))
+    /**
+     * Returns a context focused on the given node, if present,
+     * and the graph with that node removed.
+     */
+    def decomp(n: Node): Decomp[A, B] = rep.get(n) match {
+      case None => Decomp(None, this)
+      case Some(GrContext(p, label, s)) =>
+        val g1 = rep - n
+        val pp = p - n
+        val sp = s - n
+        val g2 = clearPred(g1, n, sp.keys.toVector)
+        val g3 = clearSucc(g2, n, pp.keys.toVector)
+        Decomp(Some(Context(toAdj(pp), n, label, toAdj(s))), Graph(g3))
     }
-    def addPredP(p: GrContext[A,B]) = {
-      val GrContext(ps, lp, ss) = p
-      GrContext(ps.updateWith(v, Vector(l), _ ++ _), lp, ss)
+
+    /**
+     * Merge the given context into the graph. The context consists of a vertex, its label,
+     * its successors, and its predecessors.
+     */
+    def &(ctx: Context[A,B]): Graph[A,B] = {
+      val Context(p, v, l, s) = ctx
+      val g1 = rep + (v -> GrContext(fromAdj(p), l, fromAdj(s)))
+      val g2 = addSucc(g1, v, p)
+      val g3 = addPred(g2, v, s)
+      Graph(g3)
     }
-    val g1 = adjust(rep, v, addSuccP)
-    Graph(adjust(g1, w, addPredP))
+
+    /** Add a node to this graph */
+    def addNode(n: LNode[A]): Graph[A,B] = {
+      val LNode(v, l) = n
+      Graph(rep + (v -> GrContext(IntMap.empty, l, IntMap.empty)))
+    }
+
+    /** Add an edge to this graph */
+    def addEdge(e: LEdge[B]): Graph[A,B] = {
+      val LEdge(v, w, l) = e
+      def addSuccP(p: GrContext[A,B]) = {
+        val GrContext(ps, lp, ss) = p
+        GrContext(ps, lp, ss.insertWith(w, Vector(l))(_ ++ _))
+      }
+      def addPredP(p: GrContext[A,B]) = {
+        val GrContext(ps, lp, ss) = p
+        GrContext(ps.insertWith(v, Vector(l))(_ ++ _), lp, ss)
+      }
+      val g1 = rep.alter(v)(_ map addSuccP)
+      Graph(g1.alter(w)(_ map addPredP))
+    }
+
+    /** Add multiple nodes to this graph */
+    def addNodes(vs: Seq[LNode[A]]): Graph[A,B] =
+      vs.foldLeft(this)(_ addNode _)
+
+    /** Add multiple edges to this graph */
+    def addEdges(es: Seq[LEdge[B]]): Graph[A,B] =
+      es.foldLeft(this)(_ addEdge _)
+
+    /** Remove a node from this graph */
+    def removeNode(v: Node): Graph[A,B] =
+      removeNodes(Seq(v))
+
+    /** Remove multiple nodes from this graph */
+    def removeNodes(vs: Seq[Node]): Graph[A,B] =
+      if (vs.isEmpty) this else decomp(vs.head).rest.removeNodes(vs.tail)
+
+    /** Remove an edge from this graph */
+    def removeEdge(e: Edge): Graph[A,B] = decomp(e.from) match {
+      case Decomp(None, _) => this
+      case Decomp(Some(Context(p, v, l, s)), gp) =>
+        gp & Context(p, v, l, s.filter { case (_, n) => n != e.to })
+    }
+
+    /** Remove an edge from this graph only if the label matches */
+    def removeLEdge(e: LEdge[B]): Graph[A,B] = decomp(e.from) match {
+      case Decomp(None, _) => this
+      case Decomp(Some(Context(p, v, l, s)), gp) =>
+        gp & Context(p, v, l, s.filter { case (x, n) => (x != e.label) || (n != e.to) })
+    }
+
+    /** Remove multiple edges from this graph */
+    def removeEdges(es: Seq[Edge]): Graph[A,B] =
+      es.foldLeft(this)(_ removeEdge _)
+
+    /** A list of all the nodes in the graph and their labels */
+    def labNodes: Vector[LNode[A]] =
+      rep.toVector map { case (node, GrContext(_, label, _)) => LNode(node, label) }
+
+    /** A list of all the nodes in the graph */
+    def nodes: Vector[Node] = labNodes.map(_.vertex)
+
+    /**
+     * Decompose this graph into the context for an arbitrarily chosen node
+     * and the rest of the graph.
+     */
+    def decompAny: GDecomp[A,B] = labNodes match {
+      case Vector() => sys.error("Cannot decompose an empty graph")
+      case vs =>
+        val Decomp(Some(c), g) = decomp(vs.head.vertex)
+        GDecomp(c, g)
+    }
+
+    /** The number of nodes in this graph */
+    def countNodes: Int = rep.size
+
+    /** The minimum and maximum node in a graph */
+    def nodeRange: (Node, Node) = {
+      val vs = nodes
+      (vs.min, vs.max)
+    }
+
+    /** A list of all the edges in the graph and their labels */
+    def labEdges: Vector[LEdge[B]] = for {
+      (node, GrContext(_, _, s)) <- rep.toVector
+      (next, labels) <- s.toVector
+      label <- labels
+    } yield LEdge(node, next, label)
+
+    /** A list of all the edges in the graph */
+    def edges = labEdges.map { case LEdge(v, w, _) => Edge(v, w) }
+
+    /** Fold a function over the graph */
+    def fold[C](u: C)(f: (Context[A,B], C) => C): C = {
+      val GDecomp(c, g) = decompAny
+      if (isEmpty) u else f(c, g.fold(u)(f))
+    }
+
+    /** Map a function over the graph */
+    def gmap[C,D](f: Context[A,B] => Context[C,D]): Graph[C,D] =
+      fold(empty[C,D]) { (c, g) => g & f(c) }
+
+    /** Map a function over the node labels in the grap */
+    def nmap[C](f: A => C): Graph[C, B] =
+      gmap { case Context(p, v, l, s) => Context(p, v, f(l), s) }
+
+    /** Map a function over the edge labels in the graph */
+    def emap[C](f: B => C): Graph[A, C] = {
+      def map1(f: B => C, s: Vector[(B, Node)]) =
+        s.map { case (b, n) => (f(b), n) }
+      gmap { case Context(p, v, l, s) => Context(map1(f, p), v, l, map1(f, s)) }
+    }
+
+    /** A list of `i` available nodes, i.e. nodes that are not used in the graph */
+    def freshNodes(i: Int): Vector[Node] = {
+      val (_, n) = nodeRange
+      Range(n + 1, n + i).toVector
+    }
+
+    /** Returns true if the given node is in the graph, otherwise false */
+    def gelem(v: Node): Boolean = decomp(v) match {
+      case Decomp(Some(_), _) => true
+      case _ => false
+    }
+
+    /**
+     * Find the context for the given node. Causes an error if the node is not
+     * present in the graph.
+     */
+    def context(v: Node): Context[A,B] =
+      decomp(v).ctx.getOrElse(sys.error(s"Node $v is not present in the graph"))
+
+    /** All the inbound links of the given node, including self-edges */
+    def ins(v: Node): Adj[B] =
+      context(v).ins
+
+    /** All the outbound links of the given node, including self-edges */
+    def outs(v: Node): Adj[B] =
+      context(v).outs
+
+    /** Find the label for a node */
+    def label(v: Node): Option[A] =
+      decomp(v).ctx.map(_.label)
+
+    /** Find the neighbors of a node */
+    def neighbors(v: Node): Vector[Node] = {
+      val Context(p, _, _, s) = context(v)
+      (p ++ s).map(_._2)
+    }
+
+    /** Find all nodes that have a link from the given node */
+    def successors(v: Node): Vector[Node] =
+      outs(v).map(_._2)
+
+    /** Find all nodes that have a link to the given node */
+    def predecessors(v: Node): Vector[Node] =
+      ins(v).map(_._2)
+
+    /** Find all outbound edges for the given node */
+    def outEdges(v: Node): Vector[LEdge[B]] =
+      outs(v).map { case (l, w) => LEdge(v, w, l) }
+
+    /** Find all inbound edges for the given node */
+    def inEdges(v: Node): Vector[LEdge[B]] =
+      ins(v).map { case (l, w) => LEdge(v, w, l) }
+
+    /** The number of outbound edges from the given node */
+    def outDegree(v: Node): Int =
+      outs(v).length
+
+    /** The number of inbound edges from the given node */
+    def inDegree(v: Node): Int =
+      ins(v).length
+
+    /** The number of connections to and from the given node */
+    def degree(v: Node): Int = {
+      val Context(p, _, _, s) = context(v)
+      p.length + s.length
+    }
+
   }
-
-  /** Add multiple nodes to this graph */
-  def addNodes(vs: Seq[LNode[A]]): Graph[A,B] =
-    vs.foldLeft(this)(_ addNode _)
-
-  /** Add multiple edges to this graph */
-  def addEdges(es: Seq[LEdge[B]]): Graph[A,B] =
-    es.foldLeft(this)(_ addEdge _)
-
-  /** Remove a node from this graph */
-  def removeNode(v: Node): Graph[A,B] =
-    removeNodes(Seq(v))
-
-  /** Remove multiple nodes from this graph */
-  def removeNodes(vs: Seq[Node]): Graph[A,B] =
-    if (vs.isEmpty) this else decomp(vs.head).rest.removeNodes(vs.tail)
-
-  /** Remove an edge from this graph */
-  def removeEdge(e: Edge): Graph[A,B] = decomp(e.from) match {
-    case Decomp(None, _) => this
-    case Decomp(Some(Context(p, v, l, s)), gp) =>
-      gp & Context(p, v, l, s.filter { case (_, n) => n /== e.to })
-  }
-
-  /** Remove an edge from this graph only if the label matches */
-  def removeLEdge(e: LEdge[B])(implicit eq: Equal[B]): Graph[A,B] = decomp(e.from) match {
-    case Decomp(None, _) => this
-    case Decomp(Some(Context(p, v, l, s)), gp) =>
-      gp & Context(p, v, l, s.filter { case (x, n) => (x /== e.label) || (n /== e.to) })
-  }
-
-  /** Remove multiple edges from this graph */
-  def removeEdges(es: Seq[Edge]): Graph[A,B] =
-    es.foldLeft(this)(_ removeEdge _)
-
-  /** A list of all the nodes in the graph and their labels */
-  def labNodes: Vector[LNode[A]] =
-    rep.toVector map { case (node, GrContext(_, label, _)) => LNode(node, label) }
-
-  /** A list of all the nodes in the graph */
-  def nodes: Vector[Node] = labNodes.map(_.vertex)
-
-  /**
-   * Decompose this graph into the context for an arbitrarily chosen node
-   * and the rest of the graph.
-   */
-  def decompAny: GDecomp[A,B] = labNodes match {
-    case Vector() => sys.error("Cannot decompose an empty graph")
-    case vs =>
-      val Decomp(Some(c), g) = decomp(vs.head.vertex)
-      GDecomp(c, g)
-  }
-
-  /** The number of nodes in this graph */
-  def countNodes: Int = rep.size
-
-  /** The minimum and maximum node in a graph */
-  def nodeRange: (Node, Node) = {
-    val vs = nodes
-    (vs.min, vs.max)
-  }
-
-  /** A list of all the edges in the graph and their labels */
-  def labEdges: Vector[LEdge[B]] = for {
-    (node, GrContext(_, _, s)) <- rep.toVector
-    (next, labels) <- s.toVector
-    label <- labels
-  } yield LEdge(node, next, label)
-
-  /** A list of all the edges in the graph */
-  def edges = labEdges.map { case LEdge(v, w, _) => Edge(v, w) }
-
-  /** Fold a function over the graph */
-  def fold[C](u: C)(f: (Context[A,B], C) => C): C = {
-    val GDecomp(c, g) = decompAny
-    if (isEmpty) u else f(c, g.fold(u)(f))
-  }
-
-  /** Map a function over the graph */
-  def gmap[C,D](f: Context[A,B] => Context[C,D]): Graph[C,D] =
-    fold(Graph.empty[C,D]) { (c, g) => g & f(c) }
-
-  /** Map a function over the node labels in the grap */
-  def nmap[C](f: A => C): Graph[C, B] =
-    gmap { case Context(p, v, l, s) => Context(p, v, f(l), s) }
-
-  /** Map a function over the edge labels in the graph */
-  def emap[C](f: B => C): Graph[A, C] = {
-    def map1(f: B => C, s: Vector[(B, Node)]) =
-      s.map { case (b, n) => (f(b), n) }
-    gmap { case Context(p, v, l, s) => Context(map1(f, p), v, l, map1(f, s)) }
-  }
-
-  /** A list of `i` available nodes, i.e. nodes that are not used in the graph */
-  def freshNodes(i: Int): Vector[Node] = {
-    val (_, n) = nodeRange
-    Range(n + 1, n + i).toVector
-  }
-
-  /** Returns true if the given node is in the graph, otherwise false */
-  def gelem(v: Node): Boolean = decomp(v) match {
-    case Decomp(Some(_), _) => true
-    case _ => false
-  }
-
-  /**
-   * Find the context for the given node. Causes an error if the node is not
-   * present in the graph.
-   */
-  def context(v: Node): Context[A,B] =
-    decomp(v).ctx.getOrElse(sys.error(s"Node $v is not present in the graph"))
-
-  /** Find the label for a node */
-  def label(v: Node): Option[A] =
-    decomp(v).ctx.map(_.label)
-
 }
-
