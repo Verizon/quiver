077ec9373a240e00790ad2c078e448b4a8f3880b
RunarX Bjarnason <rbjarnax@rbjarnax-mac01.local>
2013-11-04 13:19:37 -0800

    More tests

--- src/test/scala/com/intel/graphs/GraphTests.scala
+++ src/test/scala/com/intel/graphs/GraphTests.scala
@@ -2,9 +2,11 @@ package com.intel.graphs
 
 import org.scalacheck._
 import org.scalacheck.Prop._
+import scalaz.{Node => _, _}
 
 object GraphTests extends Properties("Graph") {
-  val gg = new GraphGen[Byte]
+  type Node = Byte
+  val gg = new GraphGen[Node]
   import gg._
 
   property("Adding a node to a graph yields a graph that contains that node") =
@@ -22,5 +24,26 @@ object GraphTests extends Properties("Graph") {
       g.addNode(LNode(e.from, 0)).addNode(LNode(e.to, 0)).addEdge(e).findEdge(e.edge).isDefined
     }
 
+  property("Adding an edge and removing it again yields a graph that doesn't contain that edge") =
+    forAll { (g: Graph[Int, Int], e: LEdge[Int]) =>
+      g.addNode(LNode(e.from, 0)).
+      addNode(LNode(e.to, 0)).
+      addEdge(e).
+      removeLEdge(e).
+      findEdge(e.edge).isEmpty
+    }
 
+  property("A graph constructed from nodes and edges should contain those nodes and edges") =
+    forAll { (ns: List[Node], es: List[LEdge[Int]], i: Int) =>
+      implicit val NO = Order[LNode[Int]].toScalaOrdering
+      implicit val EO = Order[LEdge[Int]].toScalaOrdering
+      val nns = ns.toSet.toList.map((b: Node) => LNode(b, i)).sorted
+      val ees = es.toSet.toList.filter((x: LEdge[Int]) =>
+                  ns.contains(x.from) && ns.contains(x.to)
+                ).map((x: LEdge[Int]) =>
+                  LEdge(x.from, x.to, i)
+                ).sorted
+      val g = mkGraph(nns, ees)
+      nns == g.labNodes.sorted && ees == g.labEdges.sorted
+    }
 }
