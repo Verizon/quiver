16e28dc99c3e6588718bb6f4ac5c64e84e54614b
Ross A. Baker <ross.baker@one.verizon.com>
2017-09-25 17:41:37 -0400

    Apply @joroKr21's fix to genGDecomp

--- core/src/test/scala/GraphGen.scala
+++ core/src/test/scala/GraphGen.scala
@@ -50,11 +50,10 @@ object GraphGen {
   implicit def arbitraryGraph[A: Arbitrary, B: Arbitrary, N: Arbitrary] =
     Arbitrary(graphGen[N,A,B])
 
-  def genGDecomp[N: Arbitrary, A: Arbitrary, B: Arbitrary]: Gen[GDecomp[N,A,B]] = for {
-    ctx <- arbitrary[Context[N,A,B]]
-    // The remainder of the graph should not reference the focus of the decomposition or its incident edges
-    g <- arbitrary[Graph[N,A,B]].map(_.removeNode(ctx.vertex))
-  } yield GDecomp(ctx, g)
+  def genGDecomp[N: Arbitrary, A: Arbitrary, B: Arbitrary]: Gen[GDecomp[N, A, B]] = for {
+    g <- graphGen[N, A, B] if !g.isEmpty
+    n <- Gen.oneOf(g.nodes)
+  } yield g.decomp(n).toGDecomp.get
 
   implicit def arbitraryGDecomp[A: Arbitrary, B: Arbitrary, N: Arbitrary]: Arbitrary[GDecomp[N, A, B]] = Arbitrary(genGDecomp[N,A,B])
   implicit def arbitraryGDecompF[A: Arbitrary, B: Arbitrary, N: Arbitrary]: Arbitrary[GDecomp[N, A, B] => A] = Arbitrary(Gen.const(_.label))
